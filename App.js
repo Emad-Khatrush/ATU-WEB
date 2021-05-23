3f8e749f55b8a25ba2b5cef0fc9bfbe92b18e007
emadkhatrush <qwe.emad@hotmail.com>
2021-05-23 20:28:40 +0300

    added signup and login routers

--- App.js
+++ App.js
@@ -1,6 +1,39 @@
-const express    = require('express'),
-      bodyParser = require("body-parser"),
-      app        = express();
+if(process.env.NODE_ENV !== "production"){
+  require("dotenv").config();
+}
+
+const express               = require('express'),
+      bodyParser            = require("body-parser"),
+      mongoose              = require("mongoose"),
+      session               = require("express-session"),
+      passport              = require("passport"),
+      localStrategy         = require("passport-local").Strategy,
+      passportLocalMongoose = require("passport-local-mongoose"),
+      app                   = express();
+
+// require models
+const User = require("./models/user");
+
+// connect mongoose
+mongoose.connect(process.env.DB_URL , {useNewUrlParser: true, useUnifiedTopology: true}, (err) => {
+  if(err) {
+    console.log(err.message);
+  } else {
+    console.log("MongoDB connected");
+  }
+});
+// passport configurations
+app.use(session({
+  secret: "ATU",
+  resave: false,
+  saveUninitialized: false,
+}));
+
+app.use(passport.initialize());
+app.use(passport.session());
+passport.use(new localStrategy(User.authenticate()));
+passport.serializeUser(User.serializeUser());
+passport.deserializeUser(User.deserializeUser());
 
 // require routers
 const indexRoute = require("./routers/index"),
@@ -12,6 +45,11 @@ const port = 3000;
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static("public"));
 app.set("view engine", "ejs");
+
+app.use(async function(req,res,next){
+  res.locals.currentUser = req.user;
+  next();
+});
 
 //-------------------------------
 //            Routes
