2c736a994b1a1ab34fbef59ee5e3f870bd319004
emadkhatrush <qwe.emad@hotmail.com>
2021-05-23 21:22:49 +0300

    create api

---
+++ routers/api.js
@@ -0,0 +1,62 @@
+const express = require("express"),
+      User    = require("../models/user"),
+      passport   = require("passport"),
+      router  = express.Router();
+
+// login POST route
+router.post("/api/login",
+passport.authenticate("local",{
+  failureRedirect: "/login"
+}), (req, res) => { return res.redirect("/")});
+// logout route
+router.get("/logout", (req, res) => {
+  req.logout();
+  res.redirect("/");
+});
+
+// signup POST route
+router.post("/api/signup", async (req, res) => {
+  try {
+      console.log(req);
+    let newUser;
+    if(req.body.type !== "store") {
+      newUser = new User({
+        username: req.body.username,
+        firstName: req.body.firstName,
+        lastName: req.body.lastName,
+        email: req.body.email,
+        phone: req.body.phone,
+        gender:  "male",
+      });
+    } else {
+      newUser = new User({
+        username: req.body.username,
+        storeName: req.body.storeName,
+        address: req.body.address,
+        userType: req.body.type,
+        email: req.body.email,
+        phone: req.body.phone,
+        gender:  "male",
+      });
+    }
+    
+    const pass1 = req.body.password;
+    const pass2 = req.body.password2;
+
+    if(pass1 !== pass2) {
+      console.log("Password not match");
+      return res.redirect("/signup");
+    }
+    User.register(newUser, req.body.password, (err, user) => {
+      if(err) {
+        console.log(err.message);
+        return res.redirect("/signup");
+      }
+      passport.authenticate("local")(req, res, () => { return res.status(200); });
+    })
+  } catch (error) {
+    console.log(err.message);
+    return res.status(404);
+  }
+});
+module.exports = router;
