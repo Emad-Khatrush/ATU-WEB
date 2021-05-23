618b1840287a2a10ed3dfdd6604055a46ca07c03
emadkhatrush <qwe.emad@hotmail.com>
2021-05-23 21:59:45 +0300

    create api

--- routers/api.js
+++ routers/api.js
@@ -45,18 +45,18 @@ router.post("/api/signup", async (req, res) => {
 
     if(pass1 !== pass2) {
       console.log("Password not match");
-      return res.redirect("/signup");
+      return res.sendStatus(500);
     }
     User.register(newUser, req.body.password, (err, user) => {
       if(err) {
         console.log(err.message);
-        return res.redirect("/signup");
+        return res.sendStatus(500);
       }
-      passport.authenticate("local")(req, res, () => { return res.status(200).send(res); });
+      passport.authenticate("local")(req, res, () => { return res.sendStatus(200); });
     })
   } catch (error) {
     console.log(err.message);
-    return res.status(404).send(error);
+    return res.status(500).send(error);
   }
 });
 module.exports = router;
