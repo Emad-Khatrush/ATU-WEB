7848428175dc01db25d8310b49de1a57a4708f5f
gökaydemir <gokaydemir99@gmail.com>
2021-06-01 23:29:42 +0300

    stores profile

--- routers/auth.js
+++ routers/auth.js
@@ -7,6 +7,9 @@ const express = require("express"),
 router.get("/login", (req, res) => {
   res.render('./auth/login');
 });
+router.get("/storesprofile", (req, res) => {
+  res.render('./auth/storesprofile');
+});
 
 // login POST route
 router.post("/login",
